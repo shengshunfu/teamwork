2b8f3ec3593ab2de07d542e55068b6b9ab57ce80
G_will <g_will@ieqi.com>
2015-04-13 22:25:46 +0800

    验证相关逻辑修改

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,18 @@ if (config('app.debug')) {
 
 Route::get('/', 'HomeController@getIndex');
 
-Route::controllers([
-	'user' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+
+/*
+| User
+*/
+Route::get('user/signup', 'Auth\AuthController@getRegister');
+Route::post('user/signup', 'Auth\AuthController@postRegister');
+
+Route::get('user/login', 'Auth\AuthController@getLogin');
+Route::post('user/login', 'Auth\AuthController@postLogin');
+
+
+// Route::controllers([
+// 	'user' => 'Auth\AuthController',
+// 	'password' => 'Auth\PasswordController',
+// ]);
