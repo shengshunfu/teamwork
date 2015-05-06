6b7db2ebd168c8db9f7e2f7396a7fe5156b91c25
G_will <g_will@ieqi.com>
2015-05-06 12:55:51 +0800

    修改验证流程

--- app/Http/Controllers/Auth/EmailAuthController.php
+++ app/Http/Controllers/Auth/EmailAuthController.php
@@ -101,15 +101,54 @@ class EmailAuthController extends Controller {
             }
             
         }
+        /*
         else {
             // check local password
             if (Hash::check($credentials['password'], $user->password)) {
                 Auth::login($user);
                 return redirect()->intended('/');
             }
-        }
+            else {
+            // check password with email again
 
-    	return redirect()->back()->withErrors(['msg' => '用户名或密码错误']);
+                $client = new Client();
+
+                // get cookie
+                $client->get( $this->mboxUrl, [
+                    'cookies' => true,
+                    'headers' => [
+                        'User-Agent' => $this->userAgent,
+                    ],
+                ]);
+
+                // check 
+                $response = $client->post( $this->mboxUrl, [
+                    'cookies' => true,
+                    'verify' => false,
+                    'headers' => [
+                        'User-Agent' => $this->userAgent,
+                    ],
+                    'body' => [
+                        'username' => $credentials['username'],
+                        'domain' => 'datartisan.com',
+                        'password' => $credentials['password'],
+                        'ssl' => 'on',
+                    ],
+                ]);
+
+                if($response->getEffectiveUrl() == $this->checkUrl){
+                    // auth success
+                    $user->password = bcrypt($credentials['password']);
+                    $user->save();
+
+                    Auth::login($user);
+                    return redirect()->intended('/');
+                }
+            }
+        }
+        */
+
+    	return redirect()->back()->withErrors(['msg' => '用户名或密码错误，如果有改动过工作邮箱密码可以和开发人员联系，清理 Teamwork 验证缓存。']);
 	}
 
 
