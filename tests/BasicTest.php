446a46532f43fed5bf73887af2f5c805899ff468
G_will <g_will@ieqi.com>
2015-05-07 09:56:17 +0800

    修改单元测试

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -1,17 +1,21 @@
 <?php
 
-class ExampleTest extends TestCase {
+class BasicTest extends TestCase {
 
 	/**
 	 * A basic functional test example.
 	 *
 	 * @return void
 	 */
-	public function testBasicExample()
+	public function testIndex()
 	{
 		$response = $this->call('GET', '/');
-
 		$this->assertEquals(302, $response->getStatusCode());
+	}
+
+	public function testError()
+	{
+		$this->assertTrue(false);
 	}
 
 }
