84e2c22fd589688ab466ddb9cd3d5af1aa89c379
G_will <g_will@ieqi.com>
2015-05-07 10:20:36 +0800

    修改单元测试

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -12,10 +12,5 @@ class BasicTest extends TestCase {
 		$response = $this->call('GET', '/');
 		$this->assertEquals(302, $response->getStatusCode());
 	}
-
-	public function testError()
-	{
-		$this->assertTrue(false);
-	}
-
+	
 }
