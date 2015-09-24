dfb50be0668f86f7206881a6778564c6a05e206d
xusimin <xusimin@datarisan.com>
2015-09-24 13:47:49 +0800

    添加与github页面的同步编辑和编写测试用例

--- resources/views/knowledge.blade.php
+++ resources/views/knowledge.blade.php
@@ -31,12 +31,16 @@
 
 <div class="row knowledge">
     <div id="sidebar" class="col-sm-3 well well-sm">
+        <a id='edit-contents' class="btn btn-sm btn-primary pull-right" href="https://github.com/Datartisan/knowledge/edit/master/contents.md">编辑目录</a>
         {!! $sidebarNavHtml !!}
     </div>
 
     <div id="document" class="col-sm-9">
         <div class="btn-group pull-right">
+            <!--
             <a class="btn btn-default" href="https://tower.im/projects/54131c44c7d842cbbdda77a9e9be30d3/lists/b304e4af1cee464d9325527a47a365e4/show/" target="_blank">问题反馈</a>
+            -->
+            <a class="btn btn-primary" href="https://github.com/Datartisan/knowledge/edit/master/{{ $page }}">编辑</a>
         </div>
 
         {!! $documentHtml !!}
@@ -55,6 +59,9 @@ $(function(){
     var locationUrlPreLength = locationHref.lastIndexOf('/');
 
     $('#sidebar a').each(function(i, a){
+        if ($(a).attr('id') == 'edit-contents') {
+            return;
+        }
         a.href = urlPre + a.href.substring(locationUrlPreLength);
     });
 
